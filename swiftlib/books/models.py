514a5a07c3c7eb7125f4f13729da5a4a5795a672
STD018 <ani.rowjee@gmail.com>
2019-06-20 14:52:01 +0530

    made authors and books model

--- swiftlib/books/models.py
+++ swiftlib/books/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    # defining authors model
+    name = models.CharField(max_length=252)
+
+    # return author name for reference
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    # defining book model
+
+    name = models.CharField(max_length=252)
+    author = models.ManyToManyField(Author)
+    isbn10 = models.CharField(max_length=10, blank=True)
+    isbn13 = models.CharField(max_length=13, blank=True)
+    costprice = models.DecimalField(decimal_places=2)
+    SKU = models.IntegerField()
+
+    # book pretty name
+    def pretty(self):
+        authors = self.authors
+        namestring = ''
+        for author in authors:
+            namestring += author.name + ' , '
+        return self.name +' by ' namestring
+
+
+
+
+
+
