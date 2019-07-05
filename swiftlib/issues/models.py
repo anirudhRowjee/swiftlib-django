b6e17cf619aec6c5f4b0531993bcbd6e462197da
mitulgarg <42147648+mitulgarg@users.noreply.github.com>
2019-07-05 20:09:22 +0530

    Update models.py
    added date_issued , date_returned , amount_payable

--- swiftlib/issues/models.py
+++ swiftlib/issues/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from datetime import date
 
 class Issue(models.Model):
-    user_issued=models.ForeignKey('students.Student', on_delete=models.CASCADE)
-    book_issued=models.ForeignKey('issues.Issue', on_delete=models.CASCADE)
+    user_issued = models.ForeignKey('students.Student', on_delete=models.CASCADE)
+    book_issued = models.ForeignKey('issues.Issue', on_delete=models.CASCADE)
+    date_issued = models.DateField(auto_now_add=True)
+    date_returned = models.DateField()
+    days = (date(date_returned)-date(date_issued)).days
+    amount_collectable = '₹'+str(days*2)
 
+    def __str__(self):
+        return self.user_issued
