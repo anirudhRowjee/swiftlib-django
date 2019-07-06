014e004a5acaad9a8c0d9d998da6096fefc9bcdb
STD018 <ani.rowjee@gmail.com>
2019-07-06 11:00:52 +0530

    Fixed model issues

--- swiftlib/issues/models.py
+++ swiftlib/issues/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-from datetime import date
 
 class Issue(models.Model):
     user_issued = models.ForeignKey('students.Student', on_delete=models.CASCADE)
     book_issued = models.ForeignKey('issues.Issue', on_delete=models.CASCADE)
     date_issued = models.DateField(auto_now_add=True)
-    date_returned = models.DateField()
-    days = (date(date_returned)-date(date_issued)).days
-    amount_collectable = '₹'+str(days*2)
+    date_returned = models.DateField(blank=True)
 
     def __str__(self):
-        return self.user_issued
+        return self.user_issued + self.book_issued + " borrowed "
+            
