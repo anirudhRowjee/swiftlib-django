bf46ce1619bb3f5439c6b3c12c5793ec455fdaa2
STD018 <ani.rowjee@gmail.com>
2019-09-24 14:25:00 +0530

    added search customization

--- swiftlib/issues/views.py
+++ swiftlib/issues/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
+from . import models as issues
+from books import models as books
+from students import models as students
 
-# Create your views here.
 
 def home(request):
-    return render(request, 'issues/issues.html')
+
+    if request.method == 'POST':
+        data = request.POST
+        search_query = data['search_query']
+        search_criteria = data['search_criteria']
+
+        # search by book name, date or user
+        if search_criteria == 'date_issued':
+
+            search_query = search_query.replace('/', '-')
+            issues_search = issues.Issue.objects.filter(date_issued=search_query)
+            context = {
+                'results' : issues_search,
+            }
+            return render(request, 'issues/issues.html', context)
+
+        elif search_criteria == 'book_issued':
+            # match the book parameters
+            pass
+        elif search_criteria == 'student_issued':
+            # match student issued to
+            pass
+
+
+
+    else:
+        return render(request, 'issues/issues.html')
 
 def issuebook(request):
     return render(request, 'issues/issues-add-form.html')
@@ -14,9 +42,5 @@ def returnbook(request):
 def bookinfo(request):
     return render(request, 'issues/issues-info-form.html')
 
-"""
-changelog-
-1. Convert issues page to show only status of books not yet returned.
-"""
 
 
