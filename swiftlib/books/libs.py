62911405891d2301b1f4dd40768920f2cedce505
Mitul Garg <mitul7hero@gmail.com>
2019-11-04 20:56:35 +0530

    Added multiple author in single string
    to accommodate multiple authors

--- swiftlib/books/libs.py
+++ swiftlib/books/libs.py
@@ -7,18 +7,35 @@ def getBookData(isbn13):
     try:
         data = isbnlib.meta(str(isbn13), service='goob')
     except goob_exceptions.NoDataForSelectorError:
-        # google books not working
+        # Google books not working
         try:
             data_open = isbnlib.meta(str(isbn13))
             data = data_open
         except exceptions.NotValidISBNError:
             return False
         return False
+    
+    length=len(data['Authors'])
+    
+    if length==1:
+        # Only one author
+        authorstring = data['Authors'][0]
+        
+    else:
+        # To get multiple author names in one element in one string
+        authorstring= ""
+        for i in range(0,length):
+            authorstring+=data['Authors'][i]
+            if i==(length-1):
+                # To avoid extra last comma
+                pass
+            else:
+                authorstring+=","
 
+               
     name = data['Title']
-    author = data['Authors'][0]
+    author = authorstring 
     isbn13 = data['ISBN-13']
 
     print([name, author, isbn13])
     return [name, author, isbn13]
-
